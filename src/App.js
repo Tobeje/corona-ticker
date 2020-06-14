66d81a90813ba1a492c3749143dcdd030d32846a
Tobeje <schmager29@gmail.com>
2020-06-14 21:44:48 +0200

    added logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import { Cards, Chart, CountryPicker } from './components';
+import { Cards, Chart, CountryPicker } from "./components";
 
-import styles from './App.module.css';
-import { fetchData } from './api';
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class App extends React.Component {
-	async componentDidMount() {
-		const data = await fetchData();
-		console.log(data);
-	}
+  state = {
+    data: {},
+  };
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
 
-	render() {
-		return (
-			<div className={styles.container}>
-				<Cards />
-				<Chart />
-				<CountryPicker />
-			</div>
-		);
-	}
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <Chart />
+        <CountryPicker />
+      </div>
+    );
+  }
 }
 
 export default App;
